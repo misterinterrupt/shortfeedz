d30275dc325ec46bec508f1528c9e419cf6c382b
Matt Howell <misterinterrupt@gmail.com>
2011-08-28 23:35:02 -0700

    add request and access token urls

--- oauth_config.js
+++ oauth_config.js
@@ -3,4 +3,6 @@ module.exports = {
   "consumer_secret" : "YOURS_HERE",
   "access_token" : "YOURS_HERE",
   "access_token_secret" : "YOURS_HERE"
+  "requestTokenUrl" = "https://api.twitter.com/oauth/request_token",
+  "accessTokenUrl" = "https://api.twitter.com/oauth/access_token",
 }
