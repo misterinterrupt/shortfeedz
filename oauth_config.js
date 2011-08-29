26349155d4119a1bdee21183db408d088eaa9abe
Matt Howell <misterinterrupt@gmail.com>
2011-08-28 23:41:35 -0700

    missed the change in comma position

--- oauth_config.js
+++ oauth_config.js
@@ -2,7 +2,7 @@ module.exports = {
   "consumer_key" : "YOURS_HERE",
   "consumer_secret" : "YOURS_HERE",
   "access_token" : "YOURS_HERE",
-  "access_token_secret" : "YOURS_HERE"
+  "access_token_secret" : "YOURS_HERE",
   "requestTokenUrl" = "https://api.twitter.com/oauth/request_token",
-  "accessTokenUrl" = "https://api.twitter.com/oauth/access_token",
+  "accessTokenUrl" = "https://api.twitter.com/oauth/access_token"
 }
