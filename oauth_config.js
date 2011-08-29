6e48713d03480acd2bb2801264a4dcc26485adbb
Matt Howell <misterinterrupt@gmail.com>
2011-08-28 23:48:12 -0700

    change key names to camelCase to make the new config plan work

--- oauth_config.js
+++ oauth_config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "consumer_key" : "YOURS_HERE",
-  "consumer_secret" : "YOURS_HERE",
-  "access_token" : "YOURS_HERE",
-  "access_token_secret" : "YOURS_HERE",
+  "consumerKey" : "YOURS_HERE",
+  "consumerSecret" : "YOURS_HERE",
+  "accessToken" : "YOURS_HERE",
+  "accessTokenSecret" : "YOURS_HERE",
   "requestTokenUrl" : "https://api.twitter.com/oauth/request_token",
   "accessTokenUrl" : "https://api.twitter.com/oauth/access_token"
 }
