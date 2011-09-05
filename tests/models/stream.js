1397cdaf5f8d1b67549040c025a04e35913b35a9
Matt Howell <misterinterrupt@gmail.com>
2011-09-05 00:50:08 -0700

    add test for Stream.update

--- tests/models/stream.js
+++ tests/models/stream.js
@@ -21,7 +21,7 @@ var streamTests = flow.define(
     if(err) throw err;
     var idType = typeof this.s1.id;
     assert.notEqual(idType, NaN, 'id was NaN');
-    assert.equal(idType, 'number', 'id was not set to a number, it was ' + idType );
+    assert.equal(idType, 'number', 'id was not set to a number, upon Stream.save it was ' + idType );
     console.log('A Stream gets an id upon Stream.save');
     this();
   },
@@ -33,9 +33,9 @@ var streamTests = flow.define(
   function testGetReturn (err, stream) {
     if(err) throw err;
     this.s2 = stream;
-    assert.equal(typeof this.s2.terms, 'string', 'prop terms missing on stream object after get');
-    assert.equal(typeof this.s2.createdAt, 'string', 'prop createdAt missing on stream object after get');
-    console.log('A stream can be retrieved from redis via Stream.get');
+    assert.equal(typeof this.s2.terms, 'string', 'prop terms missing on stream object after Stream.get');
+    assert.equal(typeof this.s2.createdAt, 'string', 'prop createdAt missing on stream object after Stream.get');
+    console.log('A stream are retrieved from redis via Stream.get');
     this();
   },
   function testGetAll (err) {
@@ -47,7 +47,27 @@ var streamTests = flow.define(
     this.allStreams = streams;
     assert.equal(Object.prototype.toString.call(this.allStreams), '[object Array]', 'An array was not returned from Stream.getAll');
     assert.equal(this.allStreams.length > 0, true, 'Stream.getAll returned 0 objects');
-    console.log('A users streams can be retrieved from redis via Stream.getAll');
+    console.log('A user\'s streams are retrieved from redis via Stream.getAll');
+    this();
+  },
+  function testUpdate (err) {
+    if(err) throw err;
+    var s3 = this.s3 = new Stream(this.client),
+    testReturn = this,
+    params = { terms:'update' },
+    update = function (err, stream) {
+        s3 = stream;
+        s3.update(params, testReturn);
+      };    
+    Stream.get(this.client, 1, update);
+  },
+  function testUpdateReturn (err, stream) {
+    if(err) throw err;
+    this.s4 = stream;
+    var updatedAtType = Object.prototype.toString.call(this.s4.updatedAt);
+    assert.equal(this.s4.terms, 'update', 'prop terms on stream object should be "update" after Stream.update, it was ' + this.s4.terms);
+    assert.equal(updatedAtType, '[object Date]', 'prop updatedAt missing on stream object after Stream.update, it was ' + updatedAtType);
+    console.log('A stream\'s props are updated via Stream.update');
     this();
   },
   function testTearDown (err) {
